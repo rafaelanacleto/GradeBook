31801f340d1098dd7c1689c68abecb1430b1b99e
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-02-17 20:24:43 -0300

    1 - ponteiros

---
+++ pointer.c
@@ -0,0 +1,12 @@
+/* Usando os operadores & e * */
+#include <stdio.h>
+
+int main() {
+
+    int a; // a é um inteiro
+    int *aPtr; // aPtr é um ponteiro para um inteiro
+
+    
+
+    return 0;
+}
