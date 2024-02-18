bcb53caba9a3389403c22fa07f63ab8867aff8e2
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-02-17 21:32:12 -0300

    constante e ponteiro

---
+++ const_pont.c
@@ -0,0 +1,13 @@
+//Converte letras minusculas em maiusculas
+
+#include <stdio.h>
+
+void converteMaiusculas(char *);
+
+int main() {
+    char palavra[] = "letras";
+
+    printf("\nA Palavra antes da conversão: %s\n", palavra);
+
+    return 0;
+}
