39016c462dacd850ffd1983f7c1ea71b2c888fa6
rafaelanacleto <rafael.ferreira.anacleto@gmail.com>
2024-02-17 21:16:12 -0300

    update ambiente windows

--- pointer.c
+++ pointer.c
@@ -6,7 +6,11 @@ int main() {
     int a; // a é um inteiro
     int *aPtr; // aPtr é um ponteiro para um inteiro
 
-    
+    a = 7;
+    aPtr = &a;
 
+    printf("O Endereco de a e %p\n"
+            "O Valor de aPtr e %p\n\n", &a, aPtr);
+    
     return 0;
 }
